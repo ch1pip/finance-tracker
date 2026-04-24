f121dff69903b9a31155c410008e15de8c350c7a
ch1pip <alexzhit82@gmail.com>
2026-04-24 21:27:16 +0100

    Smal fix

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,69 @@
+#include <iostream>
+#include <string>
 #include "FinanceManager.h"
 
 int main() {
-	FinanceManager manager;
+	FinanceManager manger;
+	int choice;
 
-	Transaction t1(100.0, "Food", "expense", "Lunch", "2026-04-23");
-	Transaction t2(2500.0, "Salary", "income", "Monthly salary", "2026-04-02");
+	while (true) {
+		std::cout << "\n=== Personal Finance Tracker ===\n";
+		std::cout << "1. Add transaction\n";
+		std::cout << "2. Show all transaction\n";
+		std::cout << "3. Exit\n";
+		std::cout << "Choose option: ";
+		std::cin >> choice;
 
-	manager.addTransaction(t1);
-	manager.addTransaction(t2);
 
-	manager.printAllTransactions();
+		if (choice == 1) {
+			double amount;
+			std::string category;
+			std::string type;
+			std::string description;
+			std::string date;
 
+			std::cout << "Enter amount: ";
+			std::cin >> amount;
+
+			std::cout << "Enter category: ";
+			std::cin >> category;
+
+			std::cout << "Enter type (income/expense): ";
+			std::cin >> type;
+
+			std::cout << "Enter description: ";
+			std::cin.ignore();
+			std::getline(std::cin, description);
+
+			std::cout << "Enter date (YYYY-MM-DD): ";
+			std::getline(std::cin, date);
+
+			Transaction newTransaction(
+					amount, 
+					category, 
+					type, 
+					description, 
+					date
+			);
+
+			manger.addTransaction(newTransaction);
+
+			std::cout << "Transaction added successfully.\n";
+		}
+
+		else if (choice == 2) {
+			manger.printAllTransactions();
+		}
+
+		else if (choice == 3) {
+			std::cout << "Eciting program...\n";
+			break;
+		}
+
+		else {
+			std::cout << "Invalid option. Try again.\n";
+		}
+	}
+        
 	return 0;
 }
